ec9883450c13efe554ae8cc01a94340cd2a58b2b
shareen.sibande@gmail.com <51477907+u14073740@users.noreply.github.com>
2019-06-25 17:15:01 +0200

    User added to the database

--- SignalRChat/DataAccess/IChatSystemStore.cs
+++ SignalRChat/DataAccess/IChatSystemStore.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiteDB;
+using System;
+using System.IO;
 
 namespace SignalRChat.DataAccess
 {
@@ -9,10 +11,36 @@ namespace SignalRChat.DataAccess
 
     class ChatSystemStore : IChatSystemStore
     {
+        public ChatSystemStore()
+        {
+            const string fileName = "ChatSystem.db";
+            bool dbExist = File.Exists(fileName);
+            _database = new LiteDatabase(fileName);
+            if (!dbExist)
+            {
+                InitializeDatabase();
+            }
+        }
+
+        private void InitializeDatabase()
+        {
+            var users = _database.GetCollection<User>("users");
+            // var channel = _database.GetCollection<Channel>("channels");
+
+            var user1 = new User
+            {
+                Name = "JohnDoe",
+                Password = "1234"
+            };
+            users.Insert(user1);
+        }
+
         public bool ValidateUser(StoreLoginCredentials loginCredentials)
         {
             throw new NotImplementedException();
+         
         }
+        private LiteDatabase _database;
     }
 
     public class StoreLoginCredentials
--- SignalRChat/DataAccess/User.cs
+++ SignalRChat/DataAccess/User.cs
@@ -7,7 +7,6 @@ namespace SignalRChat.DataAccess
 {
     public class User
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
 
