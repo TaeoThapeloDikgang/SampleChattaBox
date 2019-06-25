c8f2e3f5b69b5d67f0fbdb3707b525dd278b5d70
Taeo Thapelo Dikgang <realtepza@gmail.com>
2019-06-25 15:22:51 +0200

    added mvc

---
+++ SignalRChat/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRChat.Controllers
+{
+    public class LoginCredentials
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginCredentials credentials)
+        {
+            if (false)
+            {
+                RedirectToAction("Index", "Chat");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Login failed.";
+                return View();
+            }
+        }
+    }
+}
--- SignalRChat/Startup.cs
+++ SignalRChat/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SignalRChat.Hubs;
@@ -24,15 +25,15 @@ namespace SignalRChat
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddCors(options => options.AddPolicy("CorsPolicy",
-                builder =>
-                {
-                    builder.AllowAnyMethod().AllowAnyHeader()
-                           .WithOrigins("http://localhost:55830")
-                           .AllowCredentials();
-                }));
+            //services.AddCors(options => options.AddPolicy("CorsPolicy",
+            //    builder =>
+            //    {
+            //        builder.AllowAnyMethod().AllowAnyHeader()
+            //               .WithOrigins("http://localhost:55830")
+            //               .AllowCredentials();
+            //    }));
 
             services.AddSignalR();
         }
@@ -46,19 +47,27 @@ namespace SignalRChat
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
+                app.UseExceptionHandler("/Home/Error");
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
+
             app.UseStaticFiles();
             app.UseCookiePolicy();
-            app.UseCors("CorsPolicy");
+
+            //app.UseCors("CorsPolicy");
             app.UseSignalR(routes =>
             {
                 routes.MapHub<ChatHub>("/chathub");
             });
-            app.UseMvc();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
