9ce711b224037213162e83e0d1f2e829c43276cf
Nkosinathi M Zamisa <nkosi656@hotmail.com>
2019-06-26 10:20:00 +0200

    added id property to user

--- SignalRChat/DataAccess/User.cs
+++ SignalRChat/DataAccess/User.cs
@@ -7,8 +7,8 @@ namespace SignalRChat.DataAccess
 {
     public class User
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
-
     }
 }
