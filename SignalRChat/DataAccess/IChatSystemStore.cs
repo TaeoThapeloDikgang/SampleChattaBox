da1dd4e7a264f9ebdd9889fce5a9db108631ec00
shareen.sibande@gmail.com <51477907+u14073740@users.noreply.github.com>
2019-06-26 16:07:48 +0200

    Adding users and channels to database

--- SignalRChat/DataAccess/IChatSystemStore.cs
+++ SignalRChat/DataAccess/IChatSystemStore.cs
@@ -1,52 +1,9 @@
-﻿using LiteDB;
-using System;
-using System.IO;
+﻿using System;
 
 namespace SignalRChat.DataAccess
 {
     public interface IChatSystemStore
     {
         bool ValidateUser(StoreLoginCredentials loginCredentials);
-    }
-
-    class ChatSystemStore : IChatSystemStore
-    {
-        public ChatSystemStore()
-        {
-            const string fileName = "ChatSystem.db";
-            bool dbExist = File.Exists(fileName);
-            _database = new LiteDatabase(fileName);
-            if (!dbExist)
-            {
-                InitializeDatabase();
-            }
-        }
-
-        private void InitializeDatabase()
-        {
-            var users = _database.GetCollection<User>("users");
-            var channel = _database.GetCollection<Channel>("channels");
-
-            var user1 = new User
-            {
-                Name = "JohnDoe",
-                Password = "1234"
-            };
-            users.Insert(user1);
-        }
-
-        public bool ValidateUser(StoreLoginCredentials loginCredentials)
-        {
-            throw new NotImplementedException();
-            //var results = users.find(x => x.Name = loginCredentials.Username);
-        }
-        private LiteDatabase _database;
-        private object users;
-    }
-
-    public class StoreLoginCredentials
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
     }
 }
